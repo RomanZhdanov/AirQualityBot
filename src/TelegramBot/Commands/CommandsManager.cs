248916eb94b37c3469abb7145bd3088cd87622b3
Roman Zhdanov <zhdanov.roma@gmail.com>
2024-03-06 19:05:12 +0400

    Some optimization in commands manager.

--- src/TelegramBot/Commands/CommandsManager.cs
+++ src/TelegramBot/Commands/CommandsManager.cs
@@ -16,6 +16,16 @@ public class CommandsManager
 {
     private readonly IQAirService _airService;
     private readonly Dictionary<long, UserProfile> _usersData;
+
+    private WelcomeCommandHandler? _welcomeCommandHandler;
+    private SetUserLocationCommandHandler? _setUserLocationCommandHandler;
+    private ShowAirCommandHandler? _showAirCommandHandler;
+    private SetUserCountryCommandHandler? _setUserCountryCommandHandler;
+    private SetUserStateCommandHandler? _setUserStateCommandHandler;
+    private SetUserCityCommandHandler? _setUserCityCommandHandler;
+    private UpdateCountriesPageCommandHandler? _updateCountriesPageCommandHandler;
+    private UpdateStatesPageCommandHandler? _updateStatesPageCommandHandler;
+    private UpdateCitiesPageCommandHandler? _updateCitiesPageCommandHandler;
     
     public CommandsManager(IQAirService airService, Dictionary<long, UserProfile> usersData)
     {
@@ -27,15 +37,15 @@ public class CommandsManager
     {
         return command switch
         {
-            "/set_location" => new SetUserLocationCommandHandler(_airService),
-            "/show_air" => new ShowAirCommandHandler(_airService, _usersData),
-            "set_country" => new SetUserCountryCommandHandler(_airService, _usersData),
-            "set_state" => new SetUserStateCommandHandler(_airService, _usersData),
-            "set_city" => new SetUserCityCommandHandler(_usersData),
-            "countries_page" => new UpdateCountriesPageCommandHandler(_airService),
-            "states_page" => new UpdateStatesPageCommandHandler(_airService),
-            "cities_page" => new UpdateCitiesPageCommandHandler(_airService),
-            _ => new WelcomeCommandHandler()
+            "/set_location" => _setUserLocationCommandHandler ??= new SetUserLocationCommandHandler(_airService),
+            "/show_air" => _showAirCommandHandler ??= new ShowAirCommandHandler(_airService, _usersData),
+            "set_country" => _setUserCountryCommandHandler ??= new SetUserCountryCommandHandler(_airService, _usersData),
+            "set_state" => _setUserStateCommandHandler ??= new SetUserStateCommandHandler(_airService, _usersData),
+            "set_city" => _setUserCityCommandHandler ??= new SetUserCityCommandHandler(_usersData),
+            "countries_page" => _updateCountriesPageCommandHandler ??= new UpdateCountriesPageCommandHandler(_airService),
+            "states_page" => _updateStatesPageCommandHandler ??= new UpdateStatesPageCommandHandler(_airService),
+            "cities_page" => _updateCitiesPageCommandHandler ??= new UpdateCitiesPageCommandHandler(_airService),
+            _ => _welcomeCommandHandler ??= new WelcomeCommandHandler()
         };
     }
 }
