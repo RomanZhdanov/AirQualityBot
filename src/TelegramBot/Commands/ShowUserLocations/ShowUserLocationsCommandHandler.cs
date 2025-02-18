10d49a2b31046484427bf9935801b939a9c7bba9
Roman Zhdanov <zhdanov.roma@gmail.com>
2025-02-18 18:08:56 +0400

    Updated how locations list is displayed in telegram.

--- src/TelegramBot/Commands/ShowUserLocations/ShowUserLocationsCommandHandler.cs
+++ src/TelegramBot/Commands/ShowUserLocations/ShowUserLocationsCommandHandler.cs
@@ -1,6 +1,8 @@
 using AirBro.TelegramBot.Services;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace AirBro.TelegramBot.Commands.ShowUserLocations;
 
@@ -29,12 +31,25 @@ public class ShowUserLocationsCommandHandler : IBotCommandHandler
             return;
         }
         
+        IReplyMarkup keyboard = new ReplyKeyboardRemove();
+        var buttonRows = new List<List<InlineKeyboardButton>>();
+        
         foreach (var location in locations)
         {
-            await botClient.SendTextMessageAsync(
-                chatId: chatId,
-                text: location.ToString(),
-                cancellationToken: cancellationToken);
+            var buttonRow = new List<InlineKeyboardButton>
+            {
+                InlineKeyboardButton.WithCallbackData(location.ToString(), $"client_actions|{location.Id}")
+            };
+            
+            buttonRows.Add(buttonRow);
         }
+        
+        keyboard = new InlineKeyboardMarkup(buttonRows);
+            
+        await botClient.SendTextMessageAsync(
+            chatId: chatId,
+            text: "Your locations:",
+            replyMarkup: keyboard,
+            cancellationToken: cancellationToken);
     }
 }
